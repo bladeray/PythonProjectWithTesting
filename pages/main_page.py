3ff9daf2c8726f882252d0511eb603f639f4f746
Eugene <raycut@yandex.ru>
2020-12-06 19:40:12 +0400

    test with login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,6 @@
-from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
