b249cdf201a6952e3565e50590245bb5a2f8788e
Eugene <raycut@yandex.ru>
2020-12-09 17:41:35 +0400

    task from lesson 4.3 step 7

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.wait import WebDriverWait
+from .locators import BasePageLocators
 
 
 class BasePage:
@@ -50,3 +51,10 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
 products = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -46,3 +47,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.click_add_to_basket_button()
     page.solve_quiz_and_get_code()
     page.should_be_disappeared_success_add_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
