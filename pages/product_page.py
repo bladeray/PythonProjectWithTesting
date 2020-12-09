301d536484e1471d0518fea07c290e929fa7f113
Eugene <raycut@yandex.ru>
2020-12-09 17:34:20 +0400

    task from lesson 4.3 step 6

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,6 +11,12 @@ class ProductPage(BasePage):
         correct_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text + " has been added to your basket."
         assert self.browser.find_elements(*ProductPageLocators.SUCCESS_MESSAGES)[0].text.find(correct_message) != -1, "Success add message is not found or incorrect"
 
+    def should_not_be_success_add_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGES), "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_add_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGES), "Success message is not disappeared, but should be"
+
     def should_be_success_basket_message(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         assert self.browser.find_element(*ProductPageLocators.INFO_MESSAGES).text.find(product_price) != -1, "Basket message is not found or incorrect"
--- test_product_page.py
+++ test_product_page.py
@@ -3,14 +3,14 @@ import pytest
 from pages.product_page import ProductPage
 
 products = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+            # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+            # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+            # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+            # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+            # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+            # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+            # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+            # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
@@ -22,3 +22,27 @@ def test_guest_can_add_product_to_basket(browser, product):
     page.solve_quiz_and_get_code()
     page.should_be_success_add_message()
     page.should_be_success_basket_message()
+
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0")
+    page.open()
+    page.click_add_to_basket_button()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_add_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0")
+    page.open()
+    page.should_not_be_success_add_message()
+
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0")
+    page.open()
+    page.click_add_to_basket_button()
+    page.solve_quiz_and_get_code()
+    page.should_be_disappeared_success_add_message()
